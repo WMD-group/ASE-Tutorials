10a45b2f534e2ab483786ec41d1f4d8745d609c9
Keith Butler <k.t.butler@bath.ac.uk>
2016-02-11 12:04:38 +0000

    Added opt parser to convert_structure

--- example-input-output/convert_structure.py
+++ example-input-output/convert_structure.py
@@ -1,4 +1,17 @@
+# Useage python convert_structure.py -f <input> -o <output>
 from ase.io import read, write
+from optparse import OptionParser
 
-structure = read('958456.cif')
-write('FormatePerovskite.POSCAR', structure, format='vasp', sort=True)
+parser = OptionParser()
+parser.add_option("-f", "--file",
+                  action="store", type="string", dest="file", default="Entry.cif",
+                  help="Path to input file [default: ./Entry.cif]")
+parser.add_option("-o", "--out",
+                  action="store", type="string", dest="output", default="POSCAR.out",
+                  help="Output file [default: POSCAR.out]")
+# Add further options here
+(options, args) = parser.parse_args()
+# Ensire that the input files exist
+
+structure = read(options.file)
+write(options.output, structure, format='vasp', sort=True)
